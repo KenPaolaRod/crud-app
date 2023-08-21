8abd40c52fb97abf836e4a29e59b58110eab0aa0
KenPaolaRod <kenpaorin@gmail.com>
2023-08-21 17:01:05 -0400

    first commit, html y css terminados

---
+++ script.js
@@ -0,0 +1,5 @@
+const toDoInput = document.querySelector(".to-do");
+const textInput = document.querySelector(".description");
+const submitToDo = document.getElementById("submit-to-do");
+const submitError = document.querySelector(".input-error");
+
